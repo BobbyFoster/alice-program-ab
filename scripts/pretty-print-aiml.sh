8f4b6702b2320d45c7c128f7cc8f744d007e917a
Bobby Foster <bobby@slack-corp.com>
2016-01-20 16:42:12 -0800

    merge more aiml in alice2 https://aiml-en-us-foundation-alice.googlecode.com/files/aiml-en-us-foundation-alice.v1-9.zip

--- scripts/pretty-print-aiml.sh
+++ scripts/pretty-print-aiml.sh
@@ -1,2 +1,2 @@
 echo 'pretty-printing aiml files...'
-find . -type f -name "*.aiml" | xargs -I % sh -c 'echo %; tidy -xml -i -q -m %'
+find . -type f -name "*.aiml" | xargs -I % sh -c 'echo %; tidy -xml -i -q -m -w 240 %'
